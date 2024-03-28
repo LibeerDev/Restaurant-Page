9341615a9ce1b7d8818615afb6b82e57aca235f4
LibeerDev <libeergithub@gmail.com>
2024-03-28 22:00:03 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import './index.css';
 import { homepage } from './homepage.js'
+import { menu } from './menu.js'
+import { about } from './about.js'
+
+const content = document.getElementById('content');
 
 //implement switching button switching logic
-const navBar = document.getElementsByTagName('nav');
+const navBar = document.getElementById('navbar')
 const allButtons = navBar.querySelectorAll('button');
 
 //add event listeners
 
 allButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        handleButton();
-    } );        
+        handleButton(button.id);
+    } );            
 });
 
-function handleButton() {
-    allButtons.forEach((button) => {
+function resetContent() {
+    const allTagsInContent = content.querySelectorAll('*');
+    allTagsInContent.forEach(node => node.remove());
+}
 
-    });
+function handleButton(buttonId) {
+    resetContent();
+    if (buttonId === 'homeBtn') {
+        homepage();
+    }
+
+    else if (buttonId === 'menuBtn') {
+        menu();
+    }
+    
+    else if (buttonId === 'aboutBtn') {
+        about();
+    }
 }
 
 homepage();
